8693b3881b844560010a05c8922ad4f86942988f
Israel <israel@ludi.tiamat>
2014-05-20 11:50:51 +0300

    Fixed issue #3

--- static/assets/app.js
+++ static/assets/app.js
@@ -1,10 +1,10 @@
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
-
         reader.onload = function (e) {
-            $('#img-preview').attr('src', e.target.result);
-        }
+            $('#img-preview').css("background","url('" +
+		e.target.result + "') 50% 50% no-repeat");
+         }
         reader.readAsDataURL(input.files[0]);
     }
 }
