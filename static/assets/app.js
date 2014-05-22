528d1e07848b37c76f0c84c783bf08997410fc79
The Dod <unclezzzen@gmai.com>
2014-05-22 18:16:28 +0300

    RLY, SRSLY, FIX #3
    was losing the "contain" css in preview mode after upload

--- static/assets/app.js
+++ static/assets/app.js
@@ -2,8 +2,9 @@ function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
-            $('#img-preview').css("background","url('" +
-                e.target.result + "') 50% 50% no-repeat");
+            $('#img-preview').css(
+                "background","url('" + e.target.result + "') 50% 50% no-repeat").css(
+                'background-size','contain'); // don't know how to set both in one go :(
          }
         reader.readAsDataURL(input.files[0]);
     }
